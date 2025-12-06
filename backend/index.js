b7741038763458357ecc1578c992b93bc99088c0
bluebird00002 <ellybarikinkya@gmail.com>
2025-12-06 12:20:44 +0300

    commited

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,3 @@
 // Simple entry point that re-uses the existing backend-node server
 // Importing `backend-node/server.js` will run it (it calls app.listen)
-import '../backend-node/server.js';
+import "../backend-node/server.js";
